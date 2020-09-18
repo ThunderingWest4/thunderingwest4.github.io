333f5f4f0cdda58f1d6e83be565dc2cf2bb2d58d
ThunderingWest4 <ThunderingWest4@github.com>
2020-09-17 23:01:06 -0400

    made RNN from tutorial to generate text, to2c.txt has the book Tale of Two Cities from Project Gutenberg and the h5 file will be constantly updated with the trained weights from my kaggle notebook. i'm training it on kaggle in the background for several hours a day and consistently generating text to monitor the prediction. some major issues i ran into were weird spacings, \n's, large numbers of spaces for no reason, and many others which is all filtered out in the big "text =re.sub(.....)" around line 24 of RNN_gen.py

--- text_generation_experiments/RNN_gen.py
+++ text_generation_experiments/RNN_gen.py
@@ -1,20 +1,35 @@
+# GET DATA
+import requests as rq
+content = rq.get("http://www.gutenberg.org/cache/epub/98/pg98.txt")
+open("to2c.txt", "w", encoding="utf-8").write(content)
+
+# -------------------------------------------------------
+
+# TRAIN MODEL
+
 import tensorflow as tf
 import numpy as np
-import os
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 from string import punctuation
-import nltk
+#import nltk
+import os.path
+import time
+import pickle
+import re
 
-data = " ".join(nltk.corpus.brown.words('ck10'))
+starttime = time.time()
+#data = " ".join(nltk.corpus.brown.words())
+data = open("../input/to2ctextdata/to2c.txt", encoding="utf-8").read()
+text = re.sub(" +", " ", data.lower().translate(str.maketrans("", "", punctuation)).replace("\n", " ").replace("project gutenbergtm", ""))
+
 tf.compat.v1.enable_eager_execution(
     config=None, device_policy=None, execution_mode=None
 )
 # Apparently eager execution was turned off on my machine, turned it on
 seq_len = 100
 batch_size = 512
-epochs = 10
-text = data.lower().translate(str.maketrans("", "", punctuation))
+epochs = 5
 
 n_chars = len(text)
 vocab = sorted(set(text))
@@ -22,6 +37,9 @@ n_unique = len(vocab)
 
 char2int = {c:i for i,c in enumerate(vocab)}
 int2char = {i:c for i,c in enumerate(vocab)}
+# save these dictionaries for later generation
+pickle.dump(char2int, open(f"char2int.pickle", "wb"))
+pickle.dump(int2char, open(f"int2char.pickle", "wb"))
 
 encoded_text = np.array([char2int[x] for x in text])
 char_dataset = tf.data.Dataset.from_tensor_slices(encoded_text)
@@ -66,9 +84,67 @@ model = Sequential([
     Dense(n_unique, activation="softmax"),
 ])
 
+uploadnum = 2 #used to denote which dataset backup i'm using since i'm downloading and uploading model's h5 everytime i close computer
+
+if(os.path.exists("RNNTO2Ctextgenmodel.h5")):
+    model.load_weights(f"RNNTO2Ctextgenmodel.h5")
+elif(os.path.exists(f"../input/to2ch5-{uploadnum}/RNNTO2Ctextgenmodel.h5")):
+    model.load_weights(f"../input/to2ch5-{uploadnum}/RNNTO2Ctextgenmodel.h5")
+
 model.compile(optimizer=tf.keras.optimizers.Adam(), loss="categorical_crossentropy")
 
-# train the model
-model.fit(shuffleset, steps_per_epoch=(len(encoded_text) - seq_len) // batch_size, epochs=epochs)
-# save the model
-model.save(f"results/RNNtextgenmodel.h5")
+for epoch in range(epochs):
+    print(f"Currently on Epoch {epoch+1} out of total {epochs} epochs | {(epoch/epochs)*100}% complete")
+    # train the model
+    model.fit(shuffleset, steps_per_epoch=(len(encoded_text) - seq_len) // batch_size, epochs=1)
+    # save the model
+    model.save(f"RNNTO2Ctextgenmodel.h5")
+
+endtime = time.time()
+print("time elapsed (minutes): ", (endtime-starttime)//60)
+
+# ------------------------------------------------------------------------------------------------- #
+
+# TEST MODEL / GENERATE TEXT
+
+import numpy as np
+import tqdm
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Activation
+import pickle
+
+sequence_length = 100
+seed = "yesterday i killed a man "
+char2int = pickle.load(open(f"char2int.pickle", "rb"))
+int2char = pickle.load(open(f"int2char.pickle", "rb"))
+vocab_size = len(char2int)
+
+model = Sequential([
+    LSTM(256, input_shape=(sequence_length, vocab_size), return_sequences=True),
+    Dropout(0.3), 
+    LSTM(256), 
+    Dense(vocab_size, activation="softmax"),
+])
+model.load_weights(f"RNNTO2Ctextgenmodel.h5")
+s = seed
+n_chars = 1000 #generate 1000 characters
+generated = ""
+for i in tqdm.tqdm(range(n_chars), "Generating text"):
+    # make input seq
+    X = np.zeros((1, sequence_length, vocab_size))
+    for t, char in enumerate(seed):
+        X[0, (sequence_length-len(seed))+t, char2int[char]] = 1
+    #predict
+    predicted = model.predict(X, verbose=0)[0]
+    #convert vec2int
+    next_ind = np.argmax(predicted)
+    #convert int2char
+    next_char = int2char[next_ind]
+    #add char to results
+    generated += next_char
+    #shift seed
+    seed = seed[1:] + next_char
+    
+print("Seed: ", s)
+print("Generated text: ")
+print(generated)
